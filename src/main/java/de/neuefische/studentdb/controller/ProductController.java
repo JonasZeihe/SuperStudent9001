a90d052f939e3c3249200356d2cb63a5fe8ba4ee
Roman Kuite <roman.kuite@gmail.com>
2021-02-02 11:19:11 +0100

    Added Search Parameter and new ProductService

--- src/main/java/de/neuefische/studentdb/controller/ProductController.java
+++ src/main/java/de/neuefische/studentdb/controller/ProductController.java
@@ -2,16 +2,22 @@ package de.neuefische.studentdb.controller;
 
 import de.neuefische.studentdb.database.ProductDb;
 import de.neuefische.studentdb.model.Product;
+import de.neuefische.studentdb.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("products")
 
 public class ProductController {
     ProductDb productDb = new ProductDb();
+    ProductService productService;
 
+    public ProductController() {
+        this.productService = new ProductService(this.productDb);
+    }
 
     @GetMapping
     public List<Product> listProducts() {
@@ -19,8 +25,15 @@ public class ProductController {
         return productDb.getProductList();
     }
 
+    @GetMapping("{name}")
+    public List<Product> getProductByName(@RequestParam Optional<String> name){
+        return productService.getProductList(name.orElse(""));
+    }
+
     @PutMapping
     public Product addProduct(@RequestBody Product product) {
         return productDb.addProduct(product);
     }
+
+
 }
