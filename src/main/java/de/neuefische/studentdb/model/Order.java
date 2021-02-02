b7a2f1bdbb0777eabfd0575d57e1c1e7be6e7b26
Jothaka <jhkahle@hotmail.de>
2021-02-02 15:45:43 +0100

    Changed: Use Lombok in Order and Product

--- src/main/java/de/neuefische/studentdb/model/Order.java
+++ src/main/java/de/neuefische/studentdb/model/Order.java
@@ -1,24 +1,14 @@
 package de.neuefische.studentdb.model;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
+@Data
 public class Order {
     private int id;
     private final List<Product> orderedProducts = new ArrayList<>();
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<Product> getOrderedProducts() {
-        return orderedProducts;
-    }
 
     public Product addProduct(Product product) {
         orderedProducts.add(product);
@@ -28,27 +18,5 @@ public class Order {
     public  Product removeProduct(Product product) {
         orderedProducts.remove(product);
         return  product;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return id == order.id && Objects.equals(orderedProducts, order.orderedProducts);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, orderedProducts);
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", orderedProducts=" + orderedProducts +
-                '}';
     }
 }
--- src/main/java/de/neuefische/studentdb/model/Product.java
+++ src/main/java/de/neuefische/studentdb/model/Product.java
@@ -1,50 +1,11 @@
 package de.neuefische.studentdb.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Product {
-    String id;
-    String name;
-
-    public Product(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(id, product.id) && Objects.equals(name, product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
+    private String id;
+    private String name;
 }
